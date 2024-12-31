e3dc64a28617dfa59cb5db1da08a1125f170c4a3
DhammoDeveloper <dhammodeveloper@gmail.com>
2024-12-31 12:25:16 -0300

    Agregúe la carpeta del día seis

---
+++ 06_Day_Tuples/tuples.py
@@ -0,0 +1,53 @@
+# Tuples
+
+# Colección de diferentes tipos de datos que está ordenada y no se puede cambiar (inmutable).
+# Una vez que se crea una tupla, no podemos cambiar sus valores.
+
+# Métodos de las tuples
+
+empy_tuple = () #tuple vacía
+cont_tuple = (1, 2, 3)
+fruits = ('banana', 'orange', 'mango', 'lemon')
+
+print('banana' in fruits) # Comprobar elementos en una tuple
+
+
+print(empy_tuple, cont_tuple, fruits)
+
+# Longitud de tuples
+
+len(fruits)
+print(len(fruits))
+
+# Acceder a los elementos de una tuple
+
+firts_item = fruits[0]
+print(firts_item)
+
+# Cortar tuples 
+
+all_items = fruits[1:2]
+print(all_items)
+
+# Convertir tuples a lists
+
+lst = list(fruits)
+print(lst)
+
+# Para unir tuples, se utiliza el operador +
+# Para eliminar tuples, se utiliza del
+
+
+# Ejercicios
+
+tupla_vacia = ()
+hermanas = ('Aldana', 'Damaris')
+hermano = ('Gianlucas')
+
+
+
+
+# Comprobar
+
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Iceland' in nordic_countries)
